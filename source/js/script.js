a2b207809cd976e285bdebb37f134cd2fca48182
vjacobson <newtekk@mail.ru>
2018-05-08 20:27:41 +0600

    Добавил модальное окно

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,5 @@
+// Открытие/закрытие главного меню
+
 var menu = document.querySelector('.nav-menu');
 var menuToggle = document.querySelector('.nav-menu__toggle');
 
@@ -16,12 +18,29 @@ menuToggle.addEventListener("click", (e) => {
 
 });
 
-// $(function() {
-//   var menu = document.querySelector('.nav-menu');
-//   var menuToggle = document.querySelector('.nav-menu__toggle');
-//
-//   $(menuToggle).click(function() {
-//     console.log('Clicked!');
-//     $(menu).addClass("nav-menu--slide-down");
-//   });
-// });
+// Для вызова модального окна на главной странице
+
+var modal = document.getElementById('modalCart');
+var btn = document.getElementById("openCart");
+
+if (btn) {
+  btn.onclick = function() {
+      modal.style.display = "block";
+  }
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+}
+
+// Для вызовы модального окна на странице каталога
+
+var elements = document.querySelectorAll("a.catalog__item-icon-link");
+
+for (var i = 0; i < elements.length; i++) {
+  elements[i].addEventListener("click", function() {
+    modal.style.display = "block";
+  });
+}
