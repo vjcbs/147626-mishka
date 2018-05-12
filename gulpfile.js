e75ec9c1c5e8986e0b3a99e85561e2ff4d2f3dcb
vjacobson <newtekk@mail.ru>
2018-05-12 16:42:43 +0600

    Добавил сборку + рефакторинг

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,17 @@ var gulp = require("gulp");
 var sass = require("gulp-sass");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
-// var posthtml = require("gulp-posthtml");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
+var minify = require("gulp-csso");
 var autoprefixer = require("autoprefixer");
+var imagemin = require("gulp-imagemin");
+// var webp = require("gulp-webp");
+var rename = require("gulp-rename");
+// var svgstore = require("gulp-svgstore");
+var run = require("run-sequence");
+var del = require("del");
 var server = require("browser-sync").create();
-// var del = require("del");
 
 gulp.task("style", function() {
   gulp.src("source/sass/style.scss")
@@ -17,18 +24,67 @@ gulp.task("style", function() {
       autoprefixer()
     ]))
     .pipe(gulp.dest("source/css"))
-    .pipe(server.stream());
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("source/css"))
+    // .pipe(server.stream());
 });
 
-gulp.task("serve", ["style"], function() {
+gulp.task("html",function() {
+  return gulp.src("source/*.html")
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(gulp.dest("source"));
+});
+
+gulp.task("images", function() {
+  return gulp.src("source/img/**/*.{png,jpg}")
+  .pipe(imagemin([
+    imagemin.optipng({optimizationLevel: 3}),
+    imagemin.jpegtran({progressive: true}),
+  ]))
+  .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("webp", function() {
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("clean", function (){
+  return del("build");
+});
+
+gulp.task("serve", function() {
   server.init({
-    server: "source/",
-    notify: false,
-    open: true,
-    cors: true,
-    ui: false
+    server: "source/"
   });
 
-  gulp.watch("source/sass/**/*.{scss,sass}", ["style"]);
-  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("sass/**/*.{scss,sass}", ["style"]);
+  gulp.watch("*.html").on("change", server.reload);
+  gulp.watch("js/*.js").on("change", server.reload);
+});
+
+gulp.task("copy", function() {
+  return gulp.src([
+    "fonts/**/*.{woff,woff2}",
+    "img/**",
+    "js/**"
+  ], {
+    base: "source"
+  })
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("build", function(done) {
+  run(
+    "clean",
+    "style",
+    "html",
+    "images",
+    "copy",
+    done
+  );
 });
